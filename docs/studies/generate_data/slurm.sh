f48cd3a6be0e62b2c509e1fa993e33fb7c86f3d8
Avi Vajpeyi <avi.vajpeyi@ligo.org>
2025-06-08 12:29:35 +1000

    update mem

--- docs/studies/generate_data/slurm.sh
+++ docs/studies/generate_data/slurm.sh
@@ -5,7 +5,9 @@
 #SBATCH --time=24:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=4G
+#SBATCH --mem=32G
+
+echo "Starting job"
 
 ml gcc/11.2.0 python/3.9.6 && source /fred/oz303/avajpeyi/venvs/compas_env/bin/activate
-run_cosmic_integration out_512M -i h5out_512M.h5 -p /fred/oz101/avajpeyi/COMPAS_DATA/ -n 1
+run_cosmic_integration out_512M -i h5out_512M.h5 -p /fred/oz101/avajpeyi/COMPAS_DATA/ -n 1 -v
